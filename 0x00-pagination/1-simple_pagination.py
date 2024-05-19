df4b8332c7215ab72f96847666894f1b81542b05
Jewel <jewelbansah@rocketmail.com>
2024-05-19 16:19:51 +0000

    fix task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python3
-"""
-Implement a method named get_page that takes two integer arguments page with
-default value 1 and page_size with default value 10.
+"""Write a function named index_range that takes two integer
+arguments page and page_size.
 
-You have to use this CSV file (same as the one presented at the top of
-the project)
-Use assert to verify that both arguments are integers greater than 0.
-Use index_range to find the correct indexes to paginate the dataset
-correctly and
-return the appropriate page of the dataset (i.e. the correct list of rows).
-If the input arguments are out of range for the dataset, an empty list should
-be returned.
+The function should return a tuple of size two containing a
+start index and an end index corresponding to the range of
+indexes to return in a list for those particular pagination
+parameters.
+
+Page numbers are 1-indexed, i.e. the first page is page 1.
 """
+
+
+from typing import Tuple, List
 import csv
-from typing import List, Tuple
+import math
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Function that returns a tuple of size two
+    start index and an end index corresponding to the range of
     """
-    return ((page - 1) * page_size, page * page_size)
+    # if page is 1, start at 0 and end at page_size
+    # if page is 2, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    # if page is 3, start at ((page-1) * page_size) and
+    # end at (page_size * page)
+    return ((page-1) * page_size, page_size * page)
 
 
 class Server:
@@ -39,26 +44,20 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """return the appropriate page of the dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
-def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    """
-    Function that returns a page of the dataset
+        # get the data from the csv
+        data = self.dataset()
 
-    Parameters:
-        page (int): Page number
-        page_size (int): Number of items per page
-
-    Returns:
-        List[List]: List of rows representing the page data
-    """
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
-    
-    data = self.dataset()
-    try:
-        start, end = index_range(page, page_size)
-        return data[start:end]
-    except IndexError:
-        return []
+        try:
+            # get the index to start and end at
+            start, end = index_range(page, page_size)
+            return data[start:end]
+        except IndexError:
+            return []
