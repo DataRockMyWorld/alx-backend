ccbfe3f44310c8081c329a34b193b3e14fc41039
Jewel <jewelbansah@rocketmail.com>
2024-05-28 22:12:17 +0000

    solve task 2

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,10 +7,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """returns Hello HBNB!"""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
